45ff86631fcbc62fa55455e72b34e31c8dc8b448
Helder Doutel <heldergomesdoutel@gmail.com>
2018-03-13 22:28:45 -0300

    ...

--- senac-cn-ado01/n2m.c
+++ senac-cn-ado01/n2m.c
@@ -2,16 +2,20 @@
 #include "lib.h"
 
 int main(int argc, char *arg[]){
-    int temp;
+    int numero;
+    int base_atual;
+    int base_nova;
     if(argc < 4){
         printf("Execute da seguinte maneira: Executavel <numero a converter> <base> <nova base>\n");
     }else if(argc > 4){
         printf("Execute da seguinte maneira: Executavel <numero a converter> <base> <nova base>\n");
     }else{
-        printf("teste %s\n", arg[1]);
-        sscanf(arg[1], "%d", &temp);
-        printf("convertido %d\n", temp + 10);
-        printf("int %d\n", argc);
+        numero = atoi(arg[1]);
+        base_atual = atoi(arg[2]);
+        base_nova = atoi(arg[3]);
+        printf("numero: %d\n", numero);
+        printf("base atual: %d\n", base_atual);
+        printf("base nova: %d\n", base_nova);
     }
     
     return 0;
