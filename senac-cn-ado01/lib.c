e3e6b947286f8953610b50b5a849774a4887fa4f
Helder Doutel <heldergomesdoutel@gmail.com>
2018-03-24 15:09:15 -0300

    Versao final

--- senac-cn-ado01/lib.c
+++ senac-cn-ado01/lib.c
@@ -9,14 +9,14 @@ int elevado(float x, int y){
     return x;
 }
 
-int convertDecimal(char c){
+int char_para_int(char c){
     if (c >= '0' && c <= '9')
         return (int)c - '0';
     else
         return (int)c - 'A' + 10;
 }
 
-char convertString(int num){
+char int_para_char(int num){
     if (num >= 0 && num <= 9)
         return (char)(num + '0');
     else
@@ -31,7 +31,7 @@ int para_base10(int baseatual, char *arg){
     for(int x=0; x<len_numero; x++){
         y = y - 1;
         // printf("%c*%d^%d %d\n", arg[x], baseatual, y, elevado(baseatual, y));
-        valor += (convertDecimal(arg[x]) * elevado(baseatual, y));
+        valor += (char_para_int(arg[x]) * elevado(baseatual, y));
     }
     return valor;
 }
@@ -39,7 +39,7 @@ int para_base10(int baseatual, char *arg){
 char* de_base10(char resp[], int base_nova, int dec){
     int x;
     for(x = 0; dec > 0; x++){
-        resp[x] = convertString(dec % base_nova);
+        resp[x] = int_para_char(dec % base_nova);
         dec = dec/base_nova;
     }
     resp[x] = '\0';
--- senac-cn-ado01/n2m.c
+++ senac-cn-ado01/n2m.c
@@ -9,11 +9,35 @@ int main(int argc, char *arg[]){
     int base_nova;
     if(argc < 4){
         printf("Execute da seguinte maneira: Executavel <numero a converter> <base> <nova base>\n");
+        return 1;
     }else if(argc > 4){
         printf("Execute da seguinte maneira: Executavel <numero a converter> <base> <nova base>\n");
+        return 1;
     }else{
+        for(int x=0; x < (int) strlen(arg[2]); x++){
+            if(!(arg[2][x] >= '0' && arg[2][x] <= '9')){
+                printf("Base atual incorreta (use valores de 2 a 32)\n");
+                return 1;
+            }
+        }
         base_atual = atoi(arg[2]);
+        if(base_atual < 2 || base_atual > 32){
+            printf("Base atual incorreta (use valores de 2 a 32)\n");
+            return 1;
+        }
+
+        for(int x=0; x < (int) strlen(arg[3]); x++){
+            if(!(arg[3][x] >= '0' && arg[3][x] <= '9')){
+                printf("Base nova incorreta (use valores de 2 a 32)\n");
+                return 1;
+            }
+        }
         base_nova = atoi(arg[3]);
+        if(base_nova < 2 || base_nova > 32){
+            printf("Base nova incorreta (use valores de 2 a 32)\n");
+            return 1;
+        }
+
         if(base_nova == 10){
             printf("%d\n", para_base10(base_atual, arg[1]));
         }else if(base_atual == 10){
